e17a454849d485d2763d0447e734be6307760164
efratzadok0879 <efrat.zadok@seldatinc.com>
2018-08-29 13:24:54 +0300

    finished

--- 27.08.2018/client-angular/user-app/src/app/shared/validators/id.validator.ts
+++ 27.08.2018/client-angular/user-app/src/app/shared/validators/id.validator.ts
@@ -1,11 +1,13 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, Validators } from '@angular/forms';
 
 export function ValidateId(control: AbstractControl) {
 
     let id = control.value;
-    if (id != undefined && id != null) {
+    if (id != undefined && id != null && id.length == 9) {
         let total = 0;
         for (let i: number = 0; i < 9; i++) {
+            if (isNaN(id.charAt(i)))
+                return null;
             let x = (((i % 2) + 1) * +id.charAt(i));
             total += Math.floor(x / 10) + x % 10;
         }
